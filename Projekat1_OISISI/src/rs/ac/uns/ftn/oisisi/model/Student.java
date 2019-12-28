f20d678c7cf36883c1697e29ebfd6816dab8da1b
Luna Zivkovic <lunazivkovic@uns.ac.rs>
2019-12-28 09:59:32 +0100

    Dodavanje tabele studenata

--- Projekat1_OISISI/src/rs/ac/uns/ftn/oisisi/model/Student.java
+++ Projekat1_OISISI/src/rs/ac/uns/ftn/oisisi/model/Student.java
@@ -1,5 +1,91 @@
 package rs.ac.uns.ftn.oisisi.model;
 
-public class Student {
 
+import java.util.ArrayList;
+
+public class Student extends Osoba{
+
+	private String brojIndeksa;
+	private String datumUpisa;
+	private int godinaStudija;
+    private	String Status;
+	private double prosecnaOcena;
+	private ArrayList<Predmet> predmeti;
+	
+	
+	public Student(String ime, String prezime, String datumRodjenja, String adresaStanovanja, String email,
+			String kontakt_telefon, String brojIndeksa,String datumUpisa, int godinaStudija, String status,
+			double prosecnaOcena) {
+		super(ime, prezime, datumRodjenja, adresaStanovanja, email, kontakt_telefon);
+		this.brojIndeksa = brojIndeksa;
+		this.datumUpisa = datumUpisa;
+		this.godinaStudija = godinaStudija;
+		Status = status;
+		this.prosecnaOcena = prosecnaOcena;
+		this.predmeti = predmeti;
+	}
+
+
+	public String getBrojIndeksa() {
+		return brojIndeksa;
+	}
+
+
+	public void setBrojIndeksa(String brojIndeksa) {
+		this.brojIndeksa = brojIndeksa;
+	}
+
+
+	public String getDatumUpisa() {
+		return datumUpisa;
+	}
+
+
+	public void setDatumUpisa(String datumUpisa) {
+		this.datumUpisa = datumUpisa;
+	}
+
+
+	public int getGodinaStudija() {
+		return godinaStudija;
+	}
+
+
+	public void setGodinaStudija(int godinaStudija) {
+		this.godinaStudija = godinaStudija;
+	}
+
+
+	public String getStatus() {
+		return Status;
+	}
+
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+
+	public double getProsecnaOcena() {
+		return prosecnaOcena;
+	}
+
+
+	public void setProsecnaOcena(double prosecnaOcena) {
+		this.prosecnaOcena = prosecnaOcena;
+	}
+
+
+	public ArrayList<Predmet> getPredmeti() {
+		return predmeti;
+	}
+
+
+	public void setPredmeti(ArrayList<Predmet> predmeti) {
+		this.predmeti = predmeti;
+	}
+	
+	
+
+	
 }
---
+++ Projekat1_OISISI/src/rs/ac/uns/ftn/oisisi/view/AbstractTableModelStudenti.java
@@ -0,0 +1,54 @@
+package rs.ac.uns.ftn.oisisi.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import rs.ac.uns.ftn.oisisi.model.BazaStudenta;
+
+public class AbstractTableModelStudenti  extends AbstractTableModel {
+	
+	private static final long serialVersionUID = 2995603907983526790L;
+	public static String kolonaDugme = "DUGME";
+	public static String kolonaCheck = "CHECK";
+	
+	private List<Boolean> koJeOtkacen;
+	
+	public AbstractTableModelStudenti() {
+		this.koJeOtkacen = new ArrayList<>();
+		for (int i = 0; i < BazaStudenta.getInstance().getStudente().size(); i++) {
+			koJeOtkacen.add(false);
+		}
+	}
+
+
+	public int getRowCount() {
+	//	BazaProfesora.getInstance().getProfesore().size();
+		return 10;
+	}
+
+	@Override
+	public int getColumnCount() {
+		//return BazaProfesora.getInstance().getColumnCount() +2;
+		return BazaStudenta.getInstance().getColumnCount();
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		if (column >= BazaStudenta.getInstance().getColumnCount()) {
+			return column == 11 ? kolonaDugme : kolonaCheck;
+		}
+			return BazaStudenta.getInstance().getColumnName(column);
+	}
+
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+			return BazaStudenta.getInstance().getValueAt(rowIndex, columnIndex);
+		
+	}
+
+	
+}
